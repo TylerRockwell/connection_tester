a9990a3bcfceec41bc1d985941c0a037fab718bf
Tyler Rockwell <tyler.rockwell87@gmail.com>
2016-12-08 17:15:24 -0500

    Move connection logic to Connection class
    Fix bug where Outage object was replaced on every iteration

--- connection_test.py
+++ connection_test.py
@@ -5,32 +5,33 @@ from datetime import datetime, date
 
 class Outage:
   def __init__(self, serviceType):
-    self.startTime = currentTime()
     self.serviceType = serviceType
+    self.startTime = currentTime()
     self.endTime = None
 
   def finalize(self):
     self.endTime = currentTime()
 
-  def report(self):
-    if self.endTime != None:
-      logEvent(self.serviceType, self.startTime, self.endTime)
 
-def connectionIsActive(host="8.8.8.8", port=53, timeout=3):
-  try:
-    socket.setdefaulttimeout(timeout)
-    socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-    return True
-  except Exception as ex:
-    return False
+class Connection:
+  def __init__(self, host, port, timeout=3):
+    self.host = host
+    self.port = port
+    self.timeout = timeout
 
-def connectionStatus(WANconnected, LANconnected):
-    if WANconnected and LANconnected:
-      return closeCurrentOutage(outage)
-    elif LANconnected:
-       return buildOutage("ISP")
-    else:
-      return buildOutage("LAN")
+  def isActive(self):
+    try:
+      socket.setdefaulttimeout(self.timeout)
+      socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((self.host, self.port))
+      return True
+    except Exception as ex:
+      return False
+
+def currentTime():
+  return datetime.now().replace(microsecond=0)
+
+def outageType(LANconnected, WANconnected):
+    return 'ISP' if LANconnected else 'LAN'
 
 def buildOutage(outageType):
   return Outage(outageType)
@@ -42,43 +43,43 @@ def closeCurrentOutage(outage):
 
 def finalizeOutage(outage):
   outage.finalize()
-  outage.report()
+  logEvent(outage)
 
-def logEvent(outageType, start, end):
+def logEvent(outage):
   csv = open('outage_log.csv', 'a')
-  data = buildLogData(outageType, start, end)
+  data = buildLogData(outage)
   csv.write(','.join(data))
   csv.write("\n")
   csv.close()
 
-def buildLogData(outageType, start, end):
+def buildLogData(outage):
   return [
-           outageType,
-           readableTime(start),
-           readableTime(end),
-           str(timeDifference(start.time(), end.time()))
+           outage.serviceType,
+           readableTime(outage.startTime),
+           readableTime(outage.endTime),
+           str(timeDifference(outage.startTime.time(), outage.endTime.time()))
          ]
 
 def readableTime(inputTime):
   return inputTime.strftime("%F %r")
 
-def currentTime():
-  return datetime.now().replace(microsecond=0)
 
 def timeDifference(start, end):
   seconds = (datetime.combine(date.min, end) - datetime.combine(date.min, start)).total_seconds()
   return int(seconds)
 
 outage = None
-reporting = False
+WAN = Connection('8.8.8.8', 53)
+LAN = Connection('192.168.1.1', 53)
 print 'Connection monitoring service started'
 while True:
   try:
-    WAN = connectionIsActive()
-    LAN = connectionIsActive('192.168.1.1')
-    outage = connectionStatus(WAN, LAN)
-
-    time.sleep(5)
+    if LAN.isActive() and WAN.isActive():
+      outage = closeCurrentOutage(outage)
+    else:
+      failurePoint = outageType(LAN.isActive(), WAN.isActive())
+      outage = outage or buildOutage(failurePoint)
+    time.sleep(1)
   except KeyboardInterrupt:
     print 'Connection monitoring service stopped'
     exit(0)
